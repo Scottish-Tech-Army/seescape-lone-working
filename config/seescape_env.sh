b0a5eab75ced2645dd47c7cc97a794bd5ff822ac
Pete White <plw@plwhite.org>
2025-03-13 12:32:24 +0000

    Add seescape config

---
+++ config/seescape_env.sh
@@ -0,0 +1,23 @@
+# Parameters required for each environment.
+# Convenience variables used only by the aws CLI. Not mandatory.
+export AWS_PROFILE=seescape
+
+# This must be a valid yaml file in the same directory.
+export CONFIG_FILE=plw.yaml
+
+# Various other parameters that must be passed around into both scripts and cloudformation.
+# None of these may include spaces or special characters.
+# These variables normally have to be distinct for each environment
+export BUCKET_NAME=aws-sam-cli-managed-default-samclisourcebucket-3rhhfpjblgdf
+export ENVIRONMENT=seescape             # Environment name, used in some tags
+
+# OAuth2 flow in use. See the docs if you do not understand this. Valid values are
+# "client" and "ropc".
+export OAUTH2_FLOW=client
+
+# Number of reserved instances for performance. A good value is normally 1 or 2,
+# but you can set it to 0 if AWS quotas are preventing it working.
+export CONCURRENCY=1
+
+# These normally do not have to be distinct; leave unchanged unless you have a good reason.
+export APP=loneworker # Used for tags and naming
