acda7e29dbef12ea1c22d6d7b07007606df82e77
Pete White <plw@plwhite.org>
2025-06-19 10:13:38 +0100

    Fix up success stat in emergency call case.
    In addition, tweak test and build scripts to allow running tests outside the scope of building.

--- scripts/code_build.sh
+++ scripts/code_build.sh
@@ -6,28 +6,11 @@ set -euo pipefail
 cd "$(dirname "$0")/.."
 source scripts/utils.sh
 
-# Function to setup test environment
-setup_test_venv() {
-    echo "Setting up test virtual environment"
-    python -m venv .venv-test
-    source .venv-test/bin/activate
-
-    # Install test dependencies for all lambda functions
-    for DIR in lambdas/*/
-    do
-        if [ -f "${DIR}requirements-dev.txt" ]; then
-            pip install -r "${DIR}requirements-dev.txt"
-        fi
-    done
-}
-
 # Determine whether tests should be run based on the command line argument.
 if [ "$#" -gt 0 ]; then
     if [ "$1" = "test" ]; then
         echo "Run tests"
-        setup_test_venv
         bash scripts/code_test.sh
-        deactivate
     elif [ "$1" = "notest" ]; then
         echo "Do not run tests"
     else
@@ -55,11 +38,11 @@ do
     cp -r src/* $BUILDDIR
 
     # Create a temporary venv for installing production dependencies
-    python -m venv .venv-build
-    source .venv-build/bin/activate
+    python -m venv venv
+    source venv/bin/activate
     pip install -r requirements.txt -t $BUILDDIR
     deactivate
-    rm -rf .venv-build
+    rm -rf venv
 
     echo "Create a suitable zip file for an AWS lambda"
     cd build
--- scripts/code_test.sh
+++ scripts/code_test.sh
@@ -5,16 +5,25 @@ set -euo pipefail
 # This script must run from the parent directory of the scripts directory
 cd "$(dirname "$0")/.."
 
-# Ensure we're in the test venv
-if [[ -z "${VIRTUAL_ENV:-}" ]] || [[ ! "${VIRTUAL_ENV}" == *".venv-test" ]]; then
-    echo "Error: Must be run from within the test virtual environment"
-    exit 1
-fi
-
+# Set up the test venv, under the lambda directory.
+echo "Setting up test virtual environment and installing dependencies"
 pushd lambdas
+python -m venv venv
+source venv/bin/activate
+
+# Install test dependencies for all lambda functions
+for DIR in */
+do
+    if [ -f "${DIR}requirements-dev.txt" ]; then
+        pip install -r "${DIR}requirements-dev.txt"
+    fi
+done
 
 echo "Run tests"
 pytest -o log_cli=true -o log_cli_level=info
+
+# Deactivate the test virtual environment
+deactivate
 
 popd
 
