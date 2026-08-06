9a6e4682c7021dcfd732440d0cc1284a69951c29
Favour Odeyale <favourodeyale@gmail.com>
2026-08-06 10:23:45 +0000

    Add Vision-PK configuration files

---
+++ config/vision-pk_env.sh
@@ -0,0 +1,25 @@
+# Example configuration file, to be created for each environment.
+# Name of AWS profile to use.
+export AWS_PROFILE=my-profile
+
+# This must be a valid yaml config file, as documented in the example. It can either be:
+# - In the config directory, with no path (as here)
+# - In another directory with a relative path to the base of the repo (such as "../myconfig.yaml")
+#   This latter is to avoid email address config going into public repos.
+export CONFIG_FILE=vision-pk.yaml
+
+# Various other parameters that must be passed around into both scripts and cloudformation.
+# None of these may include spaces or special characters.
+# These variables normally have to be distinct for each environment
+export BUCKET_NAME=visionpk-loneworker-bucket
+export ENVIRONMENT=visionpk-test
+
+# Number of reserved lambda instances for performance. A good value is normally 1.
+# - Set it to 0 if AWS quotas are preventing it working or you are cost sensitive.
+# - Set it to 2 if you are using the system very heavily, and concurrent calls often happen.
+# Note that there are costs proportional to the value.
+export CONCURRENCY=0
+
+# From here on, you should probably just use the default unless you have a good reason.
+# Name of the app, used in naming pretty much all resources.
+export APP=loneworker # Used for tags and naming
